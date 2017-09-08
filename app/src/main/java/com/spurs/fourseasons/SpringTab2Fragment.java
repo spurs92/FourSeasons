f1ef3deb00aa5912f1ff8337da87fcc40966ac39
spurs <spurs92@naver.com>
2017-09-08 17:37:22 +0900

    9/8 serverLoad .....ing

--- app/src/main/java/com/spurs/fourseasons/SpringTab2Fragment.java
+++ app/src/main/java/com/spurs/fourseasons/SpringTab2Fragment.java
@@ -3,6 +3,7 @@ package com.spurs.fourseasons;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,11 +14,24 @@ import android.view.ViewGroup;
 
 public class SpringTab2Fragment extends Fragment {
 
+    SwipeRefreshLayout refreshLayout;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         View view=inflater.inflate(R.layout.spring_tab2_fragment,container,false);
+
+        refreshLayout=(SwipeRefreshLayout)view.findViewById(R.id.layout_swipe);
+
+        refreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+
+                String serverUrl="http://spursweb.dothome.co.kr/seasons/urlSaveData.php";
+
+            }
+        });
 
         return view;
     }
